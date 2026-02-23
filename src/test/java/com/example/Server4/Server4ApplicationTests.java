a3deef967e451c6b1e500cd29976da08681e8a0d
LRBDeveloper <146123222+LRBDeveloper@users.noreply.github.com>
2026-02-23 16:00:03 +0100

    .

---
+++ src/test/java/com/example/Server4/Server4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Server4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Server4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
