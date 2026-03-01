95ee6f549fe30f75ef6496aa4580f58267abd46f
Leon020785 <leonlorenzen85@gmail.com>
2026-03-01 12:58:11 +0100

    Add missing getter

--- src/main/java/com/example/Server4/entity/Problem.java
+++ src/main/java/com/example/Server4/entity/Problem.java
@@ -1,24 +1,44 @@
 package com.example.Server4.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-
+import jakarta.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
 public class Problem {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String title;
     private String description;
     private LocalDateTime createdAt;
 
-    public Problem()
-    {
+    public Problem() {
         this.createdAt = LocalDateTime.now();
+    }
 
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
     }
 }
