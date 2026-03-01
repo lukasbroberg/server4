1db7a4677e80fd21ed4935f5a0aa4288a628d0f2
Mario-Linh <conan822003@yahoo.com>
2026-03-01 15:50:40 +0100

    add user entity and repository

---
+++ src/main/java/com/example/Server4/User.java
@@ -0,0 +1,75 @@
+package com.example.Server4;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+    
+    @Column(nullable = false)
+    private String password;
+
+    
+    @Column(nullable = false)
+    private String email;
+
+     @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role = Role.USER; // mặc định là USER
+
+    // ENUM ROLE
+    public enum Role {
+        USER,
+        ADMIN
+    }
+// ===== Getter / Setter =====
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+}
