b7da9182dfa41f14cbb89c2eba7285199f96523b
Leon020785 <leonlorenzen85@gmail.com>
2026-03-13 21:42:05 +0100

    Tilføje problemstilling i back+front

--- src/main/java/com/example/Server4/ProblemController.java
+++ src/main/java/com/example/Server4/ProblemController.java
@@ -2,10 +2,12 @@ package com.example.Server4;
 
 import com.example.Server4.entity.Problem;
 import com.example.Server4.repository.ProblemRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:8081")
 @RestController
 @RequestMapping("/problems")
 public class ProblemController {
@@ -24,5 +26,15 @@ public class ProblemController {
     @PostMapping
     public Problem createProblem(@RequestBody Problem problem) {
         return problemRepository.save(problem);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProblem(@PathVariable Long id) {
+        if (!problemRepository.existsById(id)) {
+            return ResponseEntity.notFound().build();
+        }
+
+        problemRepository.deleteById(id);
+        return ResponseEntity.noContent().build();
     }
 }
